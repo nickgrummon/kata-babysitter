2ae985d59d2f6865a33a82c8fa01041ef34b0051
nickgrummon <nickgrummon@gmail.com>
2018-08-14 12:38:30 -0400

    test start to bedtime fee when bedtime is before midnight. add internal function to calculate the hours between datetime objects.

--- BabySitterFeeCalculator.py
+++ BabySitterFeeCalculator.py
@@ -13,14 +13,28 @@ class BabySitterFeeCalculator:
     gets paid for full hours (no fractional hours)
     """
     # [ Internals ]
+    def _calculate_difference_between_hours(self, endTime, initialTime):
+        """Return the difference in hours between two datetimes."""
+        latestTime = datetime.datetime.combine(datetime.date.today(), endTime)
+        earlierTime = datetime.datetime.combine(datetime.date.today(), initialTime)
+        difference = latestTime - earlierTime
+        return difference.total_seconds() / 3600
 
 
     # [ Main ]
-    def calculateFeeFromStartToBedtime(self, startTime, bedTime):
-        """Given valid start time and bed time calculate the amount of the fee."""
+    def calculateFeeFromStartToBedtime(self, startTime, bedTime=0):
+        """
+        Given valid start time and bed time calculate the amount of the fee.
+        The default value for bedtime is midnight.
+        The rate for this time period is $12/hr.
+        """
+
         start = datetime.time(hour=startTime)
         if start < datetime.time(hour=17):
             raise Exception('The start time can not be before 5pm!')
+        end = datetime.time(hour=bedTime)
+        return self._calculate_difference_between_hours(end, start) * 12
+
 
 
 FeeCalculator = BabySitterFeeCalculator()
--- BabySitterFeeCalculatorTest.py
+++ BabySitterFeeCalculatorTest.py
@@ -12,6 +12,12 @@ class TestBabySitterFeeCalulator(unittest.TestCase):
             feeCalculator.calculateFeeFromStartToBedtime(startTime=16, bedTime=22)
         self.assertTrue('The start time can not be before 5pm!' in context.exception)
 
+    def testCalculateFeeFromStartToBedtimeWhenBedtimeIsBeforeMidnight(self):
+        """When the start time is 5:00pm and bedtime is 10:00pm, calculate the correct fee."""
+        feeCalculator = BabySitterFeeCalculator()
+        calculatedFee = feeCalculator.calculateFeeFromStartToBedtime(startTime=17, bedTime=22)
+        self.assertEquals(calculatedFee, 60.0)
+
 
 if __name__ == '__main__':
     unittest.main()
